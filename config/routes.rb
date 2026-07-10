38e0da7efa8f7e3f0f546a01f823f32060625021
Mohabeb <mohabeb@gmail.com>
2026-07-10 02:03:17 -0500

    Define product category and page routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,8 @@
 Rails.application.routes.draw do
-  get "pages/show"
-  get "categories/show"
-  get "products/index"
-  get "products/show"
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  root "products#index"
 
-  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
-  # Can be used by load balancers and uptime monitors to verify that the app is live.
-  get "up" => "rails/health#show", as: :rails_health_check
+  resources :products, only: [:index, :show]
+  resources :categories, only: [:show]
 
-  # Render dynamic PWA files from app/views/pwa/*
-  get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
-  get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
-
-  # Defines the root path route ("/")
-  # root "posts#index"
+  get "pages/:slug", to: "pages#show", as: "page"
 end
