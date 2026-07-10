9f0aa2a8baec37bfbe01c14620415562117f7d70
Mohabeb <mohabeb@gmail.com>
2026-07-10 03:43:40 -0500

    Add shopping cart routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,14 @@
 Rails.application.routes.draw do
-  get "cart/show"
-  get "cart/add"
-  get "cart/update"
-  get "cart/remove"
-  get "cart/clear"
   root "products#index"
 
   resources :products, only: [:index, :show]
   resources :categories, only: [:show]
+
+  get "cart", to: "cart#show", as: "cart"
+  post "cart/add/:id", to: "cart#add", as: "add_to_cart"
+  patch "cart/update/:id", to: "cart#update", as: "update_cart"
+  delete "cart/remove/:id", to: "cart#remove", as: "remove_from_cart"
+  delete "cart/clear", to: "cart#clear", as: "clear_cart"
 
   get "pages/:slug", to: "pages#show", as: "page"
 end
