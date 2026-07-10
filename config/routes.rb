4925de55c072b03a6ccea92b5e079575347f9c41
Mohabeb <mohabeb@gmail.com>
2026-07-10 10:21:37 -0500

    Add order history route

--- config/routes.rb
+++ config/routes.rb
@@ -12,8 +12,10 @@ Rails.application.routes.draw do
   delete "cart/clear", to: "cart#clear", as: "clear_cart"
 
   get "checkout", to: "checkout#new", as: "checkout"
-  post "checkout", to: "checkout#create"
-  get "orders/:id", to: "checkout#show", as: "order"
+post "checkout", to: "checkout#create"
+
+get "orders", to: "orders#index", as: "orders"
+get "orders/:id", to: "checkout#show", as: "order"
 
   get "pages/:slug", to: "pages#show", as: "page"
 end
